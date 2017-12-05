6f49114dd94d136a1512684a27aa164667c5e7a4
pandelaz <pandelaz@gmail.com>
2017-12-05 22:03:41 +0800

    first commit

---
+++ program/ch5_p06/src/ch5_p06/ch5_p06.java
@@ -0,0 +1,12 @@
+package ch5_p06;
+
+public class ch5_p06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1 = new Car();
+		car1.show();
+	}
+
+}
---
+++ program/ch5_p14/src/pb/ch5_p14.java
@@ -0,0 +1,12 @@
+package pb;
+import pc.Car;
+
+public class ch5_p14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pc.Car car1 = new pc.Car();
+		car1.show();
+	}
+
+}
